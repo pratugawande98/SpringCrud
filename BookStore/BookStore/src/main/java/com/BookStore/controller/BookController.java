e30b9749a596634b271fda41b95c1fab803a9a5e
Pandurang Gawande <pratiksha.pandurang-gawande@capgemini.com>
2023-09-04 18:07:39 +0530

    BookStore

---
+++ BookStore/BookStore/src/main/java/com/BookStore/controller/BookController.java
@@ -0,0 +1,74 @@
+package com.BookStore.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.BookStore.entity.Book;
+import com.BookStore.entity.MyBookList;
+import com.BookStore.service.BookService;
+import com.BookStore.service.MyBookListService;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+public class BookController {
+	@Autowired
+	private BookService service;
+	
+	@Autowired
+	private MyBookListService myBookService;
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/book_register")
+	public String bookRegister() {
+		return "bookRegister";
+	}
+	@GetMapping("/available_books")
+	public ModelAndView getAllBook() {
+		List<Book>list=service.getAllBook();
+		return new ModelAndView("bookList","book",list);
+	}
+	@PostMapping("/save")
+	public String addBook(@ModelAttribute Book b) {
+		service.save(b);
+		return "redirect:/available_books";
+	}
+	@GetMapping("/my_books")
+	public String getMyBooks(Model model)
+	{
+		List<MyBookList>list=myBookService.getAllMyBooks();
+		model.addAttribute("book",list);
+		return "myBooks";
+	}
+	@RequestMapping("/mylist/{id}")
+	public String getMyList(@PathVariable("id") int id) {
+		Book b=service.getBookById(id);
+		MyBookList mb=new MyBookList(b.getId(),b.getName(),b.getAuthor(),b.getPrice());
+		myBookService.saveMyBooks(mb);
+		return "redirect:/my_books";
+	}
+	@RequestMapping("/editBook/{id}")
+	public String editBook(@PathVariable("id") int id,Model model) {
+		Book b=service.getBookById(id);
+		model.addAttribute("book",b);
+		return "bookEdit";
+	}
+	@RequestMapping("/deleteBook/{id}")
+	public String deleteBook(@PathVariable("id")int id) {
+		service.deleteById(id);
+		return "redirect:/available_books";
+	}
+
+}
